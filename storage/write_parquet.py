89955e7a424ddf5881c0e12ddebb23d0972a16a4
andrewgeday <andrewgeday@gmail.com>
2026-02-28 13:24:38 -0500

    Remove unecessary file

--- storage/write_parquet.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from pathlib import Path
-
-def write_parquet(df: pd.DataFrame, path: str, filename: str) -> None:
-    Path(path).mkdir(parents=True, exist_ok=True)
-    full_path = Path(path) / filename
-    df.to_parquet(full_path, index=False)
